b18be905c143868c23eac4487a529662eabb56d2
Jack Chuma <jack.chuma@coinbase.com>
2025-05-06 19:07:05 -0400

    improve gas price estimation and transaction status handling in oracle

--- oracle/internal/relayer/relayer.go
+++ oracle/internal/relayer/relayer.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/base/alt-l1-bridge/oracle/internal/flags"
 	localTypes "github.com/base/alt-l1-bridge/oracle/internal/types"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -59,9 +60,28 @@ func (r *Relayer) SendTransactionToBase(dep localTypes.DepositTx) error {
 		return err
 	}
 
-	gasPrice, err := r.client.SuggestGasPrice(context.Background())
+	suggestedGasPrice, err := r.client.SuggestGasPrice(context.Background())
 	if err != nil {
 		return err
+	}
+	// Increase suggested gas price by 20%
+	gasPrice := new(big.Int).Mul(suggestedGasPrice, big.NewInt(120))
+	gasPrice = new(big.Int).Div(gasPrice, big.NewInt(100))
+	log.Info("Gas price details", "suggested", suggestedGasPrice, "used", gasPrice)
+
+	balance, err := r.client.BalanceAt(context.Background(), r.address, nil)
+	if err != nil {
+		log.Warn("Failed to get account balance", "address", r.address.Hex(), "err", err)
+		// Continue even if balance check fails, but log it.
+	} else {
+		log.Info("Account balance", "address", r.address.Hex(), "balance", balance)
+		// Basic check if balance can cover gas, assuming value is zero or handled elsewhere.
+		// This is a very rough check: (gasLimit * gasPrice)
+		cost := new(big.Int).Mul(big.NewInt(int64(dep.Gas)), gasPrice)
+		if balance.Cmp(cost) < 0 {
+			log.Error("Insufficient balance for gas", "balance", balance, "estimatedCost", cost)
+			return fmt.Errorf("insufficient balance for gas: got %s, need at least %s", balance.String(), cost.String())
+		}
 	}
 
 	tx := types.NewTransaction(nonce, *dep.To, dep.Value, dep.Gas, gasPrice, dep.Data)
@@ -75,6 +95,21 @@ func (r *Relayer) SendTransactionToBase(dep localTypes.DepositTx) error {
 	err = r.client.SendTransaction(context.Background(), signedTx)
 	if err != nil {
 		return err
+	}
+
+	log.Info("Transaction submitted, waiting for receipt...", "hash", signedTx.Hash().Hex())
+
+	receipt, err := bind.WaitMined(context.Background(), r.client, signedTx)
+	if err != nil {
+		log.Error("Error waiting for transaction to be mined", "hash", signedTx.Hash().Hex(), "err", err)
+		return fmt.Errorf("error waiting for transaction to be mined: %w", err)
+	}
+
+	if receipt.Status == types.ReceiptStatusSuccessful {
+		log.Info("Transaction successfully mined", "hash", signedTx.Hash().Hex(), "blockHash", receipt.BlockHash.Hex(), "blockNumber", receipt.BlockNumber)
+	} else {
+		log.Error("Transaction failed to mine", "hash", signedTx.Hash().Hex(), "status", receipt.Status, "blockHash", receipt.BlockHash.Hex(), "blockNumber", receipt.BlockNumber)
+		return fmt.Errorf("transaction %s failed to mine with status %d", signedTx.Hash().Hex(), receipt.Status)
 	}
 
 	log.Info("Transaction sent", "hash", tx.Hash().Hex())
