bbb8b809a09a909b6547e1a70d42050e52563d0f
Jack Chuma <jack.chuma@coinbase.com>
2025-05-07 18:07:12 -0400

    simplify bridge script

--- scripts/index.ts
+++ scripts/index.ts
@@ -3,15 +3,12 @@ import { Program } from "@coral-xyz/anchor";
 import type { Bridge } from "./target/types/bridge";
 import { PublicKey } from "@solana/web3.js";
 
-const LOCAL_TOKEN_ADDRESS = new PublicKey(
-  Buffer.from(
-    "0501550155015501550155015501550155015501550155015501550155015501",
-    "hex"
-  )
+const LOCAL_TOKEN_ADDRESS = toPubkey(
+  "0501550155015501550155015501550155015501550155015501550155015501"
 );
-const REMOTE_TOKEN_ADDRESS = Uint8Array.from(
-  Buffer.from("2DBE6a59cA75EAaeC4FE78D0Cc8AAdAa6519Ce20", "hex")
-) as unknown as number[]; // wrapped SOL on Base Sepolia
+const REMOTE_TOKEN_ADDRESS = toArray(
+  "2DBE6a59cA75EAaeC4FE78D0Cc8AAdAa6519Ce20"
+); // wrapped SOL on Base Sepolia
 
 async function main() {
   const provider = anchor.AnchorProvider.env();
@@ -19,9 +16,7 @@ async function main() {
 
   const program = anchor.workspace.Bridge as Program<Bridge>;
 
-  const to = Uint8Array.from(
-    Buffer.from("9986ccaf9e3de0ffef82a0f7fa3a06d5afe07252", "hex")
-  ) as unknown as number[];
+  const to = toArray("9986ccaf9e3de0ffef82a0f7fa3a06d5afe07252");
   const value = new anchor.BN(0.001 * anchor.web3.LAMPORTS_PER_SOL);
   const minGasLimit = 100000;
   const extraData = Buffer.from("sample data payload", "utf-8");
@@ -54,3 +49,10 @@ main().catch((e) => {
   console.error(e);
   console.log(e.getLogs());
 });
+
+function toPubkey(a: string): PublicKey {
+  return new PublicKey(Buffer.from(a, "hex"));
+}
+function toArray(a: string): number[] {
+  return Uint8Array.from(Buffer.from(a, "hex")) as unknown as number[];
+}
