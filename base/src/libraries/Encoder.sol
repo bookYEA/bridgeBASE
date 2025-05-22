59d08be622b2e8f8c0ab217335996947510797e2
Jack Chuma <jack.chuma@coinbase.com>
2025-05-22 17:26:55 -0400

    encode lengths as le bytes in Base Encoder library

--- base/src/libraries/Encoder.sol
+++ base/src/libraries/Encoder.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
+import {LibBit} from "solady/utils/LibBit.sol";
+
 import {Bridge} from "../Bridge.sol";
 import {CrossChainMessenger} from "../CrossChainMessenger.sol";
 import {ISolanaMessagePasser} from "../interfaces/ISolanaMessagePasser.sol";
 
 library Encoder {
+    using LibBit for uint256;
+
     function encodeBridgePayload(Bridge.BridgePayload memory payload) internal pure returns (bytes memory) {
         return abi.encodePacked(
             payload.localToken,
@@ -13,7 +17,7 @@ library Encoder {
             payload.from,
             payload.to,
             payload.amount,
-            uint32(payload.extraData.length),
+            _getLeLength(payload.extraData.length),
             payload.extraData
         );
     }
@@ -23,13 +27,13 @@ library Encoder {
         pure
         returns (bytes memory)
     {
-        bytes memory serializedIxs = abi.encodePacked(uint32(payload.ixs.length));
+        bytes memory serializedIxs = abi.encodePacked(_getLeLength(payload.ixs.length));
 
         for (uint256 i; i < payload.ixs.length; i++) {
             serializedIxs = abi.encodePacked(serializedIxs, serializeIx(payload.ixs[i]));
         }
 
-        return abi.encodePacked(payload.nonce, payload.sender, uint32(serializedIxs.length), serializedIxs);
+        return abi.encodePacked(payload.nonce, payload.sender, _getLeLength(serializedIxs.length), serializedIxs);
     }
 
     function encodeMessage(uint256 nonce, address sender, ISolanaMessagePasser.Instruction[] memory ixs)
@@ -48,14 +52,14 @@ library Encoder {
 
     function serializeIx(ISolanaMessagePasser.Instruction memory ix) internal pure returns (bytes memory) {
         bytes memory data = abi.encodePacked(ix.programId);
-        data = abi.encodePacked(data, uint32(ix.accounts.length));
+        data = abi.encodePacked(data, _getLeLength(ix.accounts.length));
 
         for (uint256 i; i < ix.accounts.length; i++) {
             ISolanaMessagePasser.AccountMeta memory account = ix.accounts[i];
             data = abi.encodePacked(data, account.pubKey, account.isWritable, account.isSigner);
         }
 
-        data = abi.encodePacked(data, uint32(ix.data.length), ix.data);
+        data = abi.encodePacked(data, _getLeLength(ix.data.length), ix.data);
 
         return data;
     }
@@ -69,5 +73,9 @@ library Encoder {
         }
 
         return abi.encodePacked(data, ix.data);
+    }
+
+    function _getLeLength(uint256 inp) private pure returns (uint32) {
+        return uint32(inp.reverseBytes() >> 224);
     }
 }
