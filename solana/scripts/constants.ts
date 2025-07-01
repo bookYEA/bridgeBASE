8770640326c1db35ed750bbf31fc4c67e0834de9
Jack Chuma <jack.chuma@coinbase.com>
2025-07-01 15:01:35 -0400

    update solana constants and addresses

--- solana/scripts/constants.ts
+++ solana/scripts/constants.ts
@@ -1,7 +1,7 @@
 export const CONSTANTS = {
   solanaSpl: "7gpNAiU3abTrrqD4EkW5zC6Zbo3uGSzADSgsfAdQzDZ8",
-  wrappedEth: "BqpnEEWxNFsACBtF7MFddmNa8YDyPJ6ZXXGLWem58S5p",
-  wrappedERC20: "EhzEY7gwHgoDzP53qUVWkdQ5SJG48hFRD9b5ba8nLdWX",
+  wrappedEth: "J4S2C7x3ZnraP46Sav8AQh8LNfM5V9wxbEJFpkiew8Y5",
+  wrappedERC20: "FFD8WyHb6RqM5h3L8eiSx6nq2LE4nQoUrnM8ft7ufbHY",
   recipient: "0x8c1a617bdb47342f9c17ac8750e0b070c372c721",
   counterValue: "0x9a99Fc75F96983dB31523A66D53cDd50eEbfDe26",
 } as const;
