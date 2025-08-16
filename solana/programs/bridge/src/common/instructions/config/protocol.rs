f4ddeee9028964527ac8c5fcd3f6967394e32f68
Jack Chuma <jack.chuma@coinbase.com>
2025-08-16 10:17:03 -0400

    add max block interval requirement (#67)

--- solana/programs/bridge/src/common/instructions/config/protocol.rs
+++ solana/programs/bridge/src/common/instructions/config/protocol.rs
@@ -7,10 +7,21 @@ pub fn set_block_interval_requirement_handler(
     ctx: Context<SetBridgeConfig>,
     new_interval: u64,
 ) -> Result<()> {
+    require!(
+        new_interval <= 1000,
+        SetBlockIntervalRequirementError::NewIntervalTooHigh
+    );
+
     ctx.accounts
         .bridge
         .protocol_config
         .block_interval_requirement = new_interval;
 
     Ok(())
+}
+
+#[error_code]
+pub enum SetBlockIntervalRequirementError {
+    #[msg("New interval too high")]
+    NewIntervalTooHigh,
 }
